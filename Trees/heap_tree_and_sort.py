bb7ccfec053c4002cfd59853a1d9ecea1b03f10d
Kanchan Sarkar <kanchan.iiestk@gmail.com>
2020-08-17 11:27:36 +0800

    adding bits operation

--- Trees/heap_tree_and_sort.py
+++ Trees/heap_tree_and_sort.py
@@ -1,38 +1,42 @@
+import math
+
 
 class Heap:
-    def __init__(self, nums):
-        self.nums = nums
-        print (self.nums)
+    def __init__(self):
+        self.stones = list()
+        self.value = None
+        self.size = 0
 
     def heapify(self, i):
-        left = 2 * i
-        right = 2 * i + 1
-        if left < len(self.nums) and self.nums[left] > self.nums[i]:
+        left = i * 2 + 1
+        right = i * 2 + 2
+        if left < self.size and self.stones[left] > self.stones[i]:
             largest = left
         else:
             largest = i
-
-        if right < len(self.nums) and self.nums[right] > self.nums[largest]:
+        if right < self.size and self.stones[right] > self.stones[largest]:
             largest = right
 
         if largest != i:
-            temp = self.nums[largest]
-            self.nums[largest] = nums[i]
-            self.nums[i] = temp
+            temp = self.stones[largest]
+            self.stones[largest] = self.stones[i]
+            self.stones[i] = temp
             self.heapify(largest)
 
 
-    def heap_tree(self) -> list:
-        for i in range(int(len(self.nums) / 2), -1, -1):
-            self.nums = self.heapify(i)
-        return self.nums
+    def lastStoneWeight(self, stones):
+        self.stones = stones
+        self.size = len(self.stones)
+        for i in range(math.floor(len(self.stones) / 2), -1, -1):
+            self.heapify(i)
 
-    def heap_sort(self):
-        pass
+
+
+
 
 if __name__ == "__main__":
     nums = [1, 4, 20, 3, 10, 5, 33, 0]
-    hp = Heap(nums)
-    nums = hp.heap_tree()
-    print (nums)
+    hp = Heap()
+    hp.lastStoneWeight(nums)
+    print (hp.stones)
     # ans [[2, 4], [6, 6], [6, 7]]
